41434b9b606bb1a618bb507e5c756c800cb8b60a
runwiththerhythm <0h4wb7n9@students.codeinstitute.net>
2025-11-29 13:28:15 +0000

    Set views for about, contact, join

--- club/urls.py
+++ club/urls.py
@@ -1,8 +1,13 @@
+# club/urls.py
 from django.urls import path
-from .views import MembershipView
+from .views import home, MembershipView, AboutView, JoinView, ContactView
 
 app_name = "club"
 
 urlpatterns = [
+    path("", home, name="home"),
     path("membership/", MembershipView.as_view(), name="membership"),
+    path("about/", AboutView.as_view(), name="about"),
+    path("join/", JoinView.as_view(), name="join"),
+    path("contact/", ContactView.as_view(), name="contact"),
 ]
--- club/views.py
+++ club/views.py
@@ -8,3 +8,15 @@ def home(request):
 
 class MembershipView(TemplateView):
     template_name = "club/membership.html"
+
+
+class AboutView(TemplateView):
+    template_name = "club/about.html"
+
+
+class JoinView(TemplateView):
+    template_name = "club/join.html"
+
+
+class ContactView(TemplateView):
+    template_name = "club/contact.html"
