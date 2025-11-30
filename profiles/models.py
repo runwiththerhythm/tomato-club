1ba2079844e3dba9e2e1d6aeeba13fb17dbd251e
runwiththerhythm <0h4wb7n9@students.codeinstitute.net>
2025-11-30 14:03:58 +0000

    Added  profiles model

--- profiles/models.py
+++ profiles/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
 
-# Create your models here.
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    display_name = models.CharField(max_length=100, blank=True)
+    location = models.CharField(max_length=100, blank=True)
+    favourite_tomato = models.CharField(max_length=100, blank=True)
+    avatar = CloudinaryField('image', blank=True, null=True)
+
+    def __str__(self):
+        return self.user.username
